be1697ea6d212a880459868d40ca73a6176175a8
kjwong2018 <kjwong@buffalo.edu>
2017-11-18 19:18:25 -0500

    Flask Alexa Integration

--- src/app/LuxWebpage/app.py
+++ src/app/LuxWebpage/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify, render_template
-import socket, json
+from flask_ask import Ask, statement, question, session
+import socket, json, logging, threading
 
 sock = None#socket
 host = "127.0.0.1"#host address
 port = 8080
 buf_size = 1024#max message size
 dict = None#last recieved message in map format
+dev_connected = {}
 connected = False
 status_req_delim = "STOP"
 
@@ -23,6 +25,29 @@ TEST = 10
 REG_REQUEST = 11
 
 app = Flask(__name__)
+ask = Ask(app, '/')
+app.secret_key = 'secret'
+app.config['ASK_VERIFY_REQUESTS'] = False
+# logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+def send_msg_10():
+    global dev_connected
+    threading.Timer(10.0, send_msg_10).start()
+    connect()
+    command = STATUS_REQUEST
+    msg = '{"cmd":' + str(command) + ',"uuid":"0","serial":"0","data":{}}'
+    send(msg)#status_req
+    deviceDict={}
+    while (True):
+        rmsg = read().decode()
+        # print(rmsg)
+        if (rmsg == status_req_delim):
+            break
+        jsonMSG = json.loads(rmsg)
+        # print(jsonMSG)
+        deviceDict[jsonMSG["data"].get("name")] = jsonMSG.get("serial")
+    dev_connected = deviceDict
+    print(dev_connected)
 
 @app.route('/', methods=['GET'])
 @app.route('/index.html', methods=['GET'])
@@ -46,10 +71,8 @@ def status_req():
         command = STATUS_REQUEST
         msg = '{"cmd":' + str(command) + ',"uuid":"0","serial":"0","data":{}}'
         send(msg)#status_req
-
         count = 1;
         resp = "{"
-
         while (True):
             rmsg = read().decode('utf-8')
             if (rmsg == status_req_delim):
@@ -59,7 +82,7 @@ def status_req():
             resp = resp + '"' + str(count) + '" :' + rmsg
             count = count + 1
         resp = resp + "}"
-    #resp = {1:{"serial": "xg9gt5hd7651", "data": {"group_name": "Life", "name": "Lux", "level": 10}},2: {"serial": "1567hd5tg9gx", "data": {"group_name": "Baby", "name": "LuxZ", "level": 0}},3: {"serial": "1567hx", "data": {"group_name": "Life", "name": "LuxB", "level": 0}}}
+    # resp = {1:{"serial": "xg9gt5hd7651", "data": {"group_name": "Life", "name": "Lux", "level": 10}},2: {"serial": "1567hd5tg9gx", "data": {"group_name": "Baby", "name": "LuxZ", "level": 0}},3: {"serial": "1567hx", "data": {"group_name": "Life", "name": "LuxB", "level": 0}}}
     print(resp)
     #render_template('dashboard.html')
     return resp
@@ -79,7 +102,6 @@ def update_req():
 
         msg = '{"cmd":' + str(command) + ',"uuid":"' + uuid + '","serial":"' + serial + '","data":{"name":"' + name + '","level":"' + str(level) + '","group_name":"' + group + '"}}'
         send(msg)
-
         resp = read().decode('utf-8')
     return resp#render_template('dashboard.html')
 
@@ -131,5 +153,94 @@ def read():
     return msg
     #dict = json.loads(msg.decode("utf-8"))
 
+@ask.launch
+def start_skill():
+    connect()
+    welcome_msg = "Hello, welcome to Lux! If you need help, say help"
+    RPmsg = "Session will end in 8 secs.... If you need help say help"
+    print(welcome_msg)
+    return question(welcome_msg).reprompt(RPmsg)
+
+@ask.intent("OnIntent")
+def turn_on_skill(device):
+    alexa_msg = device
+    device = device.upper()
+    device = device.replace(" ","_")
+    if device in dev_connected:
+        # devName = device.replace(" ","_")
+        server_msg = '{"cmd":4,"uuid":"0","serial":"' + dev_connected[device] + '","data":{"name":"'+device+'","level":"10","group_name":"all"}}'
+        print(server_msg)
+        send(server_msg)
+        msg = alexa_msg + " is ON"
+        rcvd = read()
+    else:
+        msg = "Sorry I don't see the device you specify, please try agin."
+    RPmsg = "Session will end in 8 secs.... If you need help say help"
+    print(msg)
+    return question(msg).reprompt(RPmsg)
+
+@ask.intent("OffIntent")
+def turn_off_skill(device):
+    alexa_msg = device
+    device = device.upper()
+    device = device.replace(" ","_")
+    if device in dev_connected:
+        # devName = device.replace(" ","_")
+        server_msg = '{"cmd":4,"uuid":"0","serial":"' + dev_connected[device] + '","data":{"name":"'+device+'","level":"10","group_name":"all"}}'
+        print(server_msg)
+        send(server_msg)
+        msg = alexa_msg + " is OFF"
+        rcvd = read()
+    else:
+        msg = "Sorry I don't see the device you specify, please try agin."
+    RPmsg = "Session will end in 8 secs.... If you need help say help"
+    print(msg)
+    return question(msg).reprompt(RPmsg)
+
+@ask.intent("helpIntent")
+def help_command():
+    msg = "To turn on device, say turn on ... device name. To turn off device, say turn off ... device name."
+    RPmsg = "Session will end in 8 secs.... If you need help say help"
+    print(msg)
+    return question(msg).reprompt(RPmsg)
+
+@ask.intent("OnGroupIntent")
+def turn_on_group_skill(group):
+    if(group == "all"):
+        for device in dev_connected:
+            server_msg = '{"cmd":4,"uuid":"0","serial":"' + dev_connected[device] + '","data":{"name":"'+device+'","level":"10","group_name":"all"}}'
+            print(server_msg)
+            send(server_msg)
+            rcvd = read()
+        msg = "Group " + group + " is ON"
+    else:
+        msg = "Sorry I don't see the group you specify, please try agin."
+    RPmsg = "Session will end in 8 secs.... If you need help say help"
+    print(msg)
+    return question(msg).reprompt(RPmsg)
+
+@ask.intent("OffGroupIntent")
+def turn_off_group_skill(group):
+    if(group == "all"):
+        for device in dev_connected:
+            server_msg = '{"cmd":4,"uuid":"0","serial":"' + dev_connected[device] + '","data":{"name":"'+device+'","level":"0","group_name":"all"}}'
+            print(server_msg)
+            send(server_msg)
+            rcvd = read()
+        msg = "Group " + group + " is OFF"
+    else:
+        msg = "Sorry I don't see the group you specify, please try agin."
+    RPmsg = "Session will end in 8 secs. If you need help say help"
+    print(msg)
+    return question(msg).reprompt(RPmsg)
+
+@ask.session_ended
+def session_ended():
+    return "{}",200
+
+send_msg_10()
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
+    threaded = True
+
